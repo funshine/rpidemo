05323c0f6b0193af4c14ce61cab9a1a644f36818
Guijie Hong <guijie.hong@gmail.com>
2017-10-18 20:14:12 +0800

    add byte struct to server.py

--- warm_server/server.py
+++ warm_server/server.py
@@ -2,8 +2,15 @@
 
 import socket
 import threading
-import time
+import time, struct
+import select
+import crc
 
+def fmt2hex(str):
+    if str is not None:
+        return ':'.join(x.encode('hex') for x in str)  # python 2
+        # return ':'.join(hex(x) for x in str) # python 3
+    return None
 
 class ClientReceiveThread(threading.Thread):
 
@@ -19,19 +26,21 @@ class ClientReceiveThread(threading.Thread):
         print("Connection from : " + ip + ":" + str(port))
         self.running = True
 
-        self.socket.send("\nWelcome to the server\n\n")
+        # self.socket.send("\nWelcome to the server\n\n")
 
         data = "dummydata"
 
         try:
             while len(data) and self.running:
                 data = self.socket.recv(2048)
-                print("Receive from client " + self.ip + ":" + str(self.port) + " : " + data)
-                # self.socket.send("You sent me : " + data)
+                print("Receive from client " + self.ip + ":" + str(self.port) + " : " + fmt2hex(data))
+                # readable, writable, expt = select.select([self.socket], [], [], 5)
+                # for r in readable:
+                #     data = r.recv(2048)
+                #     print("Receive from client " + self.ip + ":" + str(self.port) + " : " + data)
+
         except socket.error as msg:
             print("ClientReceiveThread Socket Error: %s" % msg)
-        except TypeError as msg:
-            print("ClientReceiveThread Type Error: %s" % msg)
         finally:
             self.socket.close()
         print("Client disconnected...")
@@ -44,8 +53,8 @@ class ClientSendThread(threading.Thread):
         self.ip = ip
         self.port = port
         self.socket = socket
-        self.sendcount = 0
         self.running = False
+        self.data = []
         print("[+] New Send thread started for " + ip + ":" + str(port))
 
     def run(self):
@@ -56,18 +65,66 @@ class ClientSendThread(threading.Thread):
 
         try:
             while self.running:
-                data = "This is from server message number: " + str(self.sendcount)
-                self.sendcount += 1
-                print("Send to client " + self.ip + ":" + str(self.port) + " : " + data)
-                self.socket.send(data)
                 time.sleep(2)
+                if len(self.data) == 0:
+                    self.data.append(get_command(1, 0, 0, 1))
+                for d in self.data:
+                    print("Send to client " + self.ip + ":" + str(self.port) + " : " + fmt2hex(d))
+                    nsent = self.socket.send(d)
+                self.data = []
+                
         except socket.error as msg:
             print("ClientSendThread Socket Error: %s" % msg)
-        except TypeError as msg:
-            print("ClientSendThread Type Error: %s" % msg)
         finally:
             self.socket.close()
 
+    def queue(self, data):
+        self.data.append(data)
+
+class InputThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = False
+
+    def run(self):
+        self.running = True
+        func = 0x01
+        val = 0
+        while self.running:
+            vals = raw_input('Enter key, value(temperature/on/off), seperate with space: ').split()
+            print(vals)
+            if len(vals)<2:
+                print("Must provide key and value")
+                continue
+            try:
+                key = int(vals[0])
+                print(key)
+            except ValueError as ve:
+                print('Key must be numbers')
+                print(ve)
+                continue
+
+            if vals[1].upper() == 'ON':
+                func = 0x02
+                val = 1
+                print(vals[1])
+            elif vals[1].upper() == 'OFF':
+                func = 0x02
+                val = 0
+                print(vals[1])
+            else:
+                try:
+                    temp = int(vals[1])
+                    func = 0x04
+                    val = temp
+                    print(temp)
+                except ValueError as ve:
+                    print('Values must be temperature or "on"/"off"')
+                    print(ve)
+                    continue
+            data = get_command(1, key, val, func)
+            for t in sendthreads:
+                t.queue(data)
 
 class ClientThread():
     def __init__(self, ip, port, socket):
@@ -79,6 +136,16 @@ class ClientThread():
         self.sendthread.start()
         self.receivethread.start()
 
+def get_command(addr, dev, val, func):
+    # construct the bit-stream of an instruction
+    if func == 0x01:
+        PLUS = 0
+    else:
+        PLUS = 7 << 5
+    cmd = struct.pack('6B', PLUS + addr, 0x06, dev, 0x04, func, val)
+    _crc = crc.calc_string(cmd, crc.INITIAL_MODBUS)
+    _crc = ((_crc & 0XFF) << 8) + ((_crc & 0xFF00) >> 8)
+    return cmd + struct.pack('H', _crc)
 
 host = "0.0.0.0"
 port = 9898
@@ -87,24 +154,34 @@ tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 tcpsock.bind((host, port))
-threads = []
+
+recvthreads = []
+sendthreads = []
+
+cmdthread = InputThread()
+cmdthread.start()
 
 while True:
     try:
         tcpsock.listen(4)
         print("\nListening for incoming connections...")
         (clientsock, (ip, port)) = tcpsock.accept()
-        newthread = ClientThread(ip, port, clientsock)
-        newthread.start()
-        threads.append(newthread)
+        newclient = ClientThread(ip, port, clientsock)
+        newclient.start()
+        recvthreads.append(newclient.receivethread)
+        sendthreads.append(newclient.sendthread)
     except KeyboardInterrupt:
         print("User abort!")
-        for t in threads:
-            t.socket.close()
-            t.sendthread.running = False
-            t.receivethread.running = False
+        for t in recvthreads:
+            t.running = False
+        for t in sendthreads:
+            t.running = False
+        cmdthread.running = False
         break
 
-for t in threads:
-    t.sendthread.join()
-    t.receivethread.join()
+for t in recvthreads:
+    t.join()
+for t in sendthreads:
+    t.join()
+
+cmdthread.join()
--- warm_server/test_server.py
+++ warm_server/test_server.py
@@ -3,9 +3,15 @@
 
 import socket
 import sys, time
+import select
+
+def fmt2hex(str):
+    if str is not None:
+        return ':'.join(x.encode('hex') for x in str)  # python 2
+        # return ':'.join(hex(x) for x in str) # python 3
+    return None
 
 HOST, PORT = "localhost", 9898
-data = " ".join(sys.argv[1:])
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,17 +19,27 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     # Connect to server and send data
     sock.connect((HOST, PORT))
+    # sock.setblocking(0)
     sock.sendall(data + "\n")
+    received = None
 
     # Receive data from the server and shut down
     while 1:
-        received = sock.recv(1024)
-        if not received: break
-        print(received)
-        time.sleep(0.5)
-        sock.sendall(received)
+        r, w, e = select.select([sock], [], [], 10)
+        for x in r:
+            x.settimeout(5.0)
+            received = x.recv(1024)
+            if not received: break
+            x.sendall(received)
+            print(fmt2hex(received))
+        for x in w:
+            if received is not None:
+                x.sendall(received)
+            received = None
+        for x in e:
+            print("exception in select")
+            break
+        time.sleep(1)
+        
 finally:
     sock.close()
-
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
